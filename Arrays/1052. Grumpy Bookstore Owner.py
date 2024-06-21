e96515d501504fa45d6b4ff5e4439031509a0c72
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-06-22 01:20:03 +0600

    Solved

--- Arrays/1052. Grumpy Bookstore Owner.py
+++ Arrays/1052. Grumpy Bookstore Owner.py
@@ -30,16 +30,31 @@
 # Sliding Window
 
 def maxSatisfied(customers,grumpy,minutes):
+    #T.C= O(N)
+    #S.C = O(1)
+    initial_satisfaction = 0
+    max_extra_satisfaction = 0
+    current_window_satisfaction = 0
 
-    customers = []
-    for i in range(1,10):
-        customers.append(i)
+    for i in range(len(customers)):
+        if grumpy[i] == 0:
+            initial_satisfaction += customers[i]
+        elif i < minutes:
+            current_window_satisfaction += customers[i]
 
+    max_extra_satisfaction = current_window_satisfaction
 
-    return customers
+    for i in range(minutes, len(customers)):
+        current_window_satisfaction += customers[i] * grumpy[i]
+        current_window_satisfaction -= customers[i - minutes] * grumpy[i - minutes]
+        max_extra_satisfaction = max(max_extra_satisfaction, current_window_satisfaction)
 
-customer = [12,23,43]
-n=[]
-j = 0
-print(maxSatisfied(customer,n,j))
+    return initial_satisfaction + max_extra_satisfaction
+
+customers = [1,0,1,2,1,1,7,5]
+grumpy = [0,1,0,1,0,1,0,1]
+minutes = 3
+
+print(maxSatisfied(customers,grumpy,minutes))
+
 
